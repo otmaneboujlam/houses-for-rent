a2bef3059b9bd29691c9f43cb3527984b414a4d9
Otmane Boujlam <otmane.boujlam@gmail.com>
2023-09-21 23:55:56 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hfr-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'houses-for-rent';
+}
