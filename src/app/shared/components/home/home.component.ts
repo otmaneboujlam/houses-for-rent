c7e3826e96336229d58d2244168a3ff1d1105872
Otmane Boujlam <otmane.boujlam@gmail.com>
2023-09-24 16:04:04 +0200

    Fin

--- src/app/shared/components/home/home.component.ts
+++ src/app/shared/components/home/home.component.ts
@@ -13,10 +13,25 @@ export class HomeComponent {
 
   housingLocationList: HousingLocation[] = [];
 
+  filteredLocationList: HousingLocation[] = [];
+
   housingService: HousingService = inject(HousingService);
 
-  constructor(){
-    this.housingLocationList = this.housingService.getAllHousingLocations();
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
+
+  constructor() {
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
   }
 
 }
